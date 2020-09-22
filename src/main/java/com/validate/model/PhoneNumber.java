a7d6bcaab78f44f6bf05296ba120c5b93039fbb2
huy8895 <huy8895@gmail.com>
2020-09-22 14:35:25 +0700

    [Thực hành] Custom Validation  Tạo Class PhoneNumber  Thêm cấu hình file ghi lỗi cho class PhoneNumber trong file AppConfiguration   Thêm nội dung file ValidationMessages.properties  Tạo Controller PhoneController  Tạo view index

---
+++ src/main/java/com/validate/model/PhoneNumber.java
@@ -0,0 +1,45 @@
+package com.validate.model;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+
+@Component
+public class PhoneNumber implements Validator {
+
+    private String number;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    //- supports là phương thức xác định class này được Validate
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return PhoneNumber.class.isAssignableFrom(clazz);
+    }
+
+    //- validate(Object target, Errors errors) là phương thức dùng để xác minh cho đối tượng và trả về lỗi trong errors nếu có
+    @Override
+    public void validate(Object target, Errors errors) {
+
+        PhoneNumber phoneNumber = (PhoneNumber) target;
+        String number = phoneNumber.getNumber();
+        ValidationUtils.rejectIfEmpty(errors, "number", "number.empty");
+        if (number.length()>11 || number.length()<10){
+            errors.rejectValue("number", "number.length");
+        }
+        if (!number.startsWith("0")){
+            errors.rejectValue("number", "number.startsWith");
+        }
+        if (!number.matches("(^$|[0-9]*$)")){
+            errors.rejectValue("number", "number.matches");
+        }
+    }
+}
